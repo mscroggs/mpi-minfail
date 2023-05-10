07b4e5a1d14ca2e3c08a63f6b5d72f0c1dc2d6e1
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2023-05-10 09:11:09 +0100

    copy communicator from RLST

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN apt-get update
 RUN apt-get -y install libopenmpi-dev libopenblas-dev liblapack-dev libclang-dev \
     git curl pkg-config libssl-dev build-essential
 RUN curl https://sh.rustup.rs -sSf | bash -s -- -y
-RUN git clone https://github.com/mscroggs/mpi-minfail.git
+RUN echo "!" && git clone https://github.com/mscroggs/mpi-minfail.git
 RUN cd mpi-minfail && RUST_BACKTRACE=full $HOME/.cargo/bin/cargo build
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
-pub unsafe fn test() -> *mut i32 {
-    mpi_sys::MPI_UNWEIGHTED
+use mpi::environment::Universe;
+use mpi::raw::AsRaw;
+
+pub fn test() -> mpi::topology::UserCommunicator {
+    let universe: Universe = mpi::initialize().unwrap();
+    let comm = universe.world();
+    let mut neighbors = Vec::<i32>::new();
+    neighbors.push(0);
+
+    unsafe {
+        let mut raw_comm = mpi_sys::RSMPI_COMM_NULL;
+        mpi_sys::MPI_Dist_graph_create_adjacent(
+            comm.as_raw(),
+            neighbors.len() as i32,
+            neighbors.as_ptr(),
+            mpi_sys::MPI_UNWEIGHTED,
+            neighbors.len() as i32,
+            neighbors.as_ptr(),
+            mpi_sys::MPI_UNWEIGHTED,
+            mpi_sys::RSMPI_INFO_NULL,
+            0,
+            &mut raw_comm,
+        );
+
+        mpi::topology::UserCommunicator::from_raw(raw_comm).unwrap()
+    }
 }
